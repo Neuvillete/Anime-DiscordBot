cd41c2902faaa400b469f8424cbde876fba8d653
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-07-28 01:09:11 +0530

    Update charQuery.py

--- queries/charQuery.py
+++ queries/charQuery.py
@@ -1,1 +1,26 @@
+def searchchar():
+    query = '''
+    query ($search: String) {
+      Character(search: $search) {
+        siteUrl
+        name {
+          full
+        }
+        media(perPage: 1) {
+          nodes {
+            title {
+                romaji
+                english
+            }
+            siteUrl
+          }
+        }
+        image {
+          large
+        }
+        description(asHtml: true)
+      }
+    }
+    '''
+    return query
 
