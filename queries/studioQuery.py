13b35b0298e2bb64a5d230706b60550dd0ea1938
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-07-28 01:10:22 +0530

    Update studioQuery.py

--- queries/studioQuery.py
+++ queries/studioQuery.py
@@ -1,1 +1,20 @@
 
+def searchstudio():
+    query = '''
+    query ($search: String) {
+    Studio(search: $search) {
+        name
+        siteUrl
+        media (isMain: true, sort: POPULARITY_DESC, perPage: 10) {
+            nodes {
+                siteUrl
+                title {
+                    english
+                    romaji
+                }
+            }
+        }
+    }
+}
+    '''
+    return query
