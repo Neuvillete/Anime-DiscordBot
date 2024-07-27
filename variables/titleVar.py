f21b8c8fe74e839b8931e7269f17ac72c8dc3e73
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-07-27 22:47:41 +0530

    Update titleVar.py

--- variables/titleVar.py
+++ variables/titleVar.py
@@ -1,1 +1,10 @@
+def getbytitle(type, title):
+    type = type.upper()
+    if type != 'ANIME' and type != 'MANGA':
+        return False
+    variables = {
+        'type': type,
+        'search': title
+    }
+    return variables
 
