95a7b1892ac2f244613af12c45ab011b3510ef7d
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-07-28 01:08:39 +0530

    Update clean.py

--- misc/clean.py
+++ misc/clean.py
@@ -1,1 +1,17 @@
+def removetags(text):
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
+def cutlength(text):
+    if len(text) > 900:
+        return text[:900] + "..."
+    return text
+
+
+def replacenone(text):
+    if text is None:
+        return 'N/A'
+    return text
 
