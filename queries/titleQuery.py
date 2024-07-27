a0d0b61f82e614013f6a13fbb0df6d6487d7658c
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-07-28 01:10:51 +0530

    Update titleQuery.py

--- queries/titleQuery.py
+++ queries/titleQuery.py
@@ -1,1 +1,48 @@
+def searchbytitle():
+    query = '''
+    query($search: String, $type: MediaType) {
+        Media(search: $search, type: $type) {
+        title
+        {
+            romaji
+            english
+        }
+        siteUrl
+        type
+        format
+        genres
+        status
+        episodes
+        duration
+        status
+        description(asHtml: true)
+        coverImage {
+            large
+        }
+        season
+        seasonYear
+        startDate {
+            day
+            month
+            year
+        }
+        averageScore
+        favourites
+        studios
+        {
+            edges
+            {
+                node
+                {
+                    name
+                }
+            }
+        }
+        chapters
+        volumes
+        hashtag
+        }
+        }
+    '''
+    return query
 
