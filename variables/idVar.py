3029db6ef3f7de25584a66d7f355d1923f9dbf72
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-07-27 22:46:35 +0530

    Update idVar.py

--- variables/idVar.py
+++ variables/idVar.py
@@ -1,1 +1,9 @@
+def getbyid(type, id):
+    type = type.upper()
+    if type == 'ANIME' and type != 'MANGA':
+        variables = {
+            'type': type,
+            'id': id
+        }
+        return variables
 
